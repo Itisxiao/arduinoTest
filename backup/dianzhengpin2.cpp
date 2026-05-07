eb92711f87351d390c2592f49d3f8499b50b4b45
chenjx11 <1870040934@qq.com>
2026-05-07 18:26:43 +0800

    使用库完成点阵屏

---
+++ backup/dianzhengpin2.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <MD_Parola.h>
+#include <MD_MAX72xx.h>
+#include <SPI.h>
+
+// 引脚定义 (根据实际接线修改)
+// DIN -> Data In, CLK -> Clock, CS -> Chip Select (LOAD)
+#define DATA_PIN  11
+#define CLK_PIN   13
+#define CS_PIN    10
+#define MAX_DEVICES 4 
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+
+MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+
+void setup() {
+    P.begin();
+    P.displayText("deng", PA_CENTER, 100, 0, PA_PRINT, PA_NO_EFFECT);
+}
+
+void loop() {
+    P.displayAnimate();
+}
