49a9acf2587353bfaf5aa1183b61e46031c64517
chenjx11 <1870040934@qq.com>
2026-05-09 12:45:40 +0800

    点阵屏库

--- backup/dianzhengpin2.cpp
+++ backup/dianzhengpin2.cpp
@@ -3,6 +3,9 @@
 #include <MD_MAX72xx.h>
 #include <SPI.h>
 
+//majicDesigns/MD_Parola@^3.7.0
+//majicDesigns/MD_MAX72xx@^3.5.1
+
 // 引脚定义 (根据实际接线修改)
 // DIN -> Data In, CLK -> Clock, CS -> Chip Select (LOAD)
 #define DATA_PIN  11
